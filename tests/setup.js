7b579d21a62e8761957d30155b7914452d8f38d6
NotDumbj <145663898+NotDumbj@users.noreply.github.com>
2026-03-27 20:22:25 +0500

    updated yml

--- tests/setup.js
+++ tests/setup.js
@@ -4,14 +4,14 @@ const chrome = require("selenium-webdriver/chrome");
 async function buildDriver() {
     let options = new chrome.Options();
 
+    options.addArguments("--no-sandbox");
+    options.addArguments("--disable-dev-shm-usage");
+    options.addArguments("--headless");
     options.addArguments("--disable-notifications");
     options.addArguments("--disable-infobars");
     options.addArguments("--disable-save-password-bubble"); // 🔥 IMPORTANT
     options.addArguments("--disable-password-manager-reauthentication");
     options.addArguments("--disable-features=PasswordLeakDetection");
-    options.addArguments("--headless");
-    options.addArguments("--no-sandbox");
-    options.addArguments("--disable-dev-shm-usage");
 
     return await new Builder()
         .forBrowser("chrome")
